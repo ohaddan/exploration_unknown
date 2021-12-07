5abab86831c2ff068a89a04b4dc4e19d26b13c94
Ohad Dan <ohad.dan@gmail.com>
2021-12-07 12:08:23 -0500

    Minimal example for webapp

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Ohad!</p>"
+    return "<p>Hello, Ohad Dan!</p>"
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
